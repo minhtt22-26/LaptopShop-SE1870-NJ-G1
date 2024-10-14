e517a76701677f103e086c40b9779c10dd3ce7a5
SangLT03 <sanglthe173039@fpt.edu.vn>
2024-10-14 23:13:47 +0700

    fix manager order

--- src/java/controller/SelectStatusOrder.java
+++ src/java/controller/SelectStatusOrder.java
@@ -2,16 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package controller;
 
 import dal.OderDAO;
 import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.time.LocalDateTime;
 import java.util.List;
 import model.Order;
 
@@ -19,42 +20,35 @@ import model.Order;
  *
  * @author ADMIN
  */
-@WebServlet(name = "ManagerOrder", urlPatterns = {"/managerOrder"})
-public class ManagerOrder extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+@WebServlet(name="SelectStatusOrder", urlPatterns={"/selectStatusOrder"})
+public class SelectStatusOrder extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+    throws ServletException, IOException {
+        String op = request.getParameter("op");
         OderDAO oDAO = new OderDAO();
-        List<Order> listOrder = oDAO.getAllOrder();
-
-        Integer oid = (Integer) request.getAttribute("oid");
-
-        if (!(oid == null)) {
-            Order order = oDAO.getOrderByID(oid);
-            if (order.getOrderStatus().equals("done")) {
-                oDAO.updateEnddate(LocalDateTime.now(), oid);
-            }
+        List<Order> listOrder = null;
+        if (op.equals("all")) {
+           listOrder = oDAO.getAllOrder();
+        }else {
+           listOrder = oDAO.getOrderByOrderStatus(op);
         }
-
         
         request.setAttribute("listOrder", listOrder);
+        request.setAttribute("op", op);
         request.getRequestDispatcher("managerOrderDisplay.jsp").forward(request, response);
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -62,13 +56,12 @@ public class ManagerOrder extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
-    }
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -76,13 +69,12 @@ public class ManagerOrder extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -29,6 +29,10 @@ public class DBContext {
             String user = "sa";
             String pass = "123";
             String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SWP391_LaptopShop";
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
